2ab6cf78d849a70efc6b741e16f25ed8a10ae6d1
Martin <20106234@mail.wit.ie>
2023-10-07 14:52:51 +0200

    Create Users table object and update getAll function

--- src/main/kotlin/ie/setu/domain/User.kt
+++ src/main/kotlin/ie/setu/domain/User.kt
@@ -4,4 +4,5 @@ data class User(
     var id: Int,
     var name: String,
     var email: String,
-    var level: String)
+    var level: String
+)
--- src/main/kotlin/ie/setu/domain/repository/UserDAO.kt
+++ src/main/kotlin/ie/setu/domain/repository/UserDAO.kt
@@ -1,43 +1,41 @@
 package ie.setu.domain.repository
 
 import ie.setu.domain.User
+import ie.setu.domain.db.Users
+import ie.setu.utils.mapToUser
+import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.transactions.transaction
 import java.util.*
 import kotlin.collections.ArrayList
 
 
 class UserDAO {
 
-    private val users = arrayListOf<User>(
-        User(name = "Alice", email = "alice@wonderland.com", id = 0, level = "user"),
-        User(name = "Bob", email = "bob@cat.ie", id = 1, level = "user"),
-        User(name = "Mary", email = "mary@contrary.com", id = 2, level = "user"),
-        User(name = "Carol", email = "carol@singer.com", id = 3, level = "user")
-    )
-
     fun getAll() : ArrayList<User>{
-        return users
+        val userList: ArrayList<User> = arrayListOf()
+        transaction {
+            Users.selectAll().map {
+                userList.add(mapToUser(it))
+            }
+        }
+        return userList
     }
 
     fun findById(id: Int): User? {
-        return users.find { it.id == id }
+        return null
     }
 
     fun findByEmail(email: String) :User? {
-        return users.find { it.email.lowercase(Locale.getDefault()) == email.lowercase(Locale.getDefault()) }
+        return null
+        //return users.find { it.email.lowercase(Locale.getDefault()) == email.lowercase(Locale.getDefault()) }
     }
 
     fun save(user: User) {
-        users.add(user)
     }
 
     fun delete(id: Int) {
-        users.removeIf { it.id == id }
     }
 
     fun update(id: Int, user: User) {
-        val foundUser = findById(id)
-        foundUser?.email = user.email
-        foundUser?.name = user.name
-        foundUser?.id = user.id
     }
 }
