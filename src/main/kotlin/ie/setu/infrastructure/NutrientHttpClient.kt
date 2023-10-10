5b5f4dc77a8c8602b65f9230dbd19e9ec51d658c
Martin <20106234@mail.wit.ie>
2023-10-10 21:04:02 +0200

    Reformat whitespace in query parameter

--- src/main/kotlin/ie/setu/infrastructure/NutrientHttpClient.kt
+++ src/main/kotlin/ie/setu/infrastructure/NutrientHttpClient.kt
@@ -2,7 +2,7 @@ package ie.setu.infrastructure
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
-import ie.setu.domain.Ingredient
+import ie.setu.domain.FoodItem
 import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
@@ -11,16 +11,17 @@ import java.net.http.HttpResponse.BodyHandlers
 object NutrientHttpClient {
     private val mapper = jacksonObjectMapper()
 
-    fun get(mealName: String): Array<Ingredient> {
+    fun get(mealName: String): Array<FoodItem> {
+        val formattedQueryString = mealName.replace(" ", "%20")
         val client = HttpClient.newHttpClient()
         val request = HttpRequest.newBuilder()
-            .uri(URI("https://api.api-ninjas.com/v1/nutrition?query=$mealName"))
+            .uri(URI("https://api.api-ninjas.com/v1/nutrition?query=$formattedQueryString"))
             .header("X-Api-Key", "Ye2v3N4QgCjtVmqLha+19Q==AQlXfNXQg9vyPp5f")
             .GET()
             .build()
 
         val response = client.send(request, BodyHandlers.ofString())
-        val meals = mapper.readValue<Array<Ingredient>>(response.body())
+        val meals = mapper.readValue<Array<FoodItem>>(response.body())
 
         return meals
     }
