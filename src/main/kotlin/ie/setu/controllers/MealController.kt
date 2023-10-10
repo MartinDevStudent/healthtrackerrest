0755c6ee33d7727d484a512dbc7d19e4c10a4f0a
Martin <20106234@mail.wit.ie>
2023-10-10 20:59:42 +0200

    Reformat white space in query parameter

--- src/main/kotlin/ie/setu/controllers/MealController.kt
+++ src/main/kotlin/ie/setu/controllers/MealController.kt
@@ -3,19 +3,19 @@ package ie.setu.controllers
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import ie.setu.domain.MealRequest
-import ie.setu.domain.repository.MealDAO
+import ie.setu.domain.repository.FoodItemDAO
 import ie.setu.infrastructure.NutrientHttpClient
 import io.javalin.http.Context
 
 object MealController {
-    private val mealDao = MealDAO()
+    private val foodItemDao = FoodItemDAO()
 
     fun getAllMeals(ctx: Context) {
-        ctx.json(mealDao.getAll())
+        ctx.json(foodItemDao.getAll())
     }
 
     fun getMealById(ctx: Context) {
-        val meal = mealDao.findById(ctx.pathParam("meal-id").toInt())
+        val meal = foodItemDao.findById(ctx.pathParam("meal-id").toInt())
         if (meal != null) {
             ctx.json(meal)
         }
@@ -24,7 +24,11 @@ object MealController {
     fun addMeal(ctx: Context) {
         val mapper = jacksonObjectMapper()
         val meal = mapper.readValue<MealRequest>(ctx.body())
-        NutrientHttpClient.get(meal.name)
-        ctx.json(meal)
+        val food_items = NutrientHttpClient.get(meal.name)
+
+        if (food_items.count() > 0) {
+            food_items.forEach { foodItemDao.save(it) }
+            ctx.json(meal)
+        }
     }
 }
