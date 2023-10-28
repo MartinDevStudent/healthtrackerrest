1583863d4e50471d10de3913128141ad79225912
Martin <20106234@mail.wit.ie>
2023-10-28 13:55:15 +0200

    Complete directed exercises

--- src/main/kotlin/ie/setu/utils/JSONUtilities.kt
+++ src/main/kotlin/ie/setu/utils/JSONUtilities.kt
@@ -1,7 +1,12 @@
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.joda.JodaModule
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
+import kong.unirest.HttpResponse
+import kong.unirest.JsonNode
 
 /**
  * Jackson object mapper with JodaModule for date handling and custom configuration.
@@ -31,3 +36,14 @@ inline fun <reified T: Any> jsonToObject(json: String) : T
     .registerModule(JodaModule())
     .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
     .readValue<T>(json)
+
+inline fun <reified T: Any>  jsonNodeToObject(jsonNode : HttpResponse<JsonNode>) : T {
+    return jsonToObject<T>(jsonNode.body.toString())
+}
+
+fun jsonObjectMapper(): ObjectMapper
+        = ObjectMapper()
+    .registerModule(JavaTimeModule())
+    .registerModule(JodaModule())
+    .registerModule(KotlinModule.Builder().build())
+    .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
